63be994ee9ea9c5a72cf3fcfde07d0935b472736
rijarandria <rijandrisolo@gmail.com>
2023-11-07 14:42:55 +0100

    Rectification affichage résulata tp05 operation

--- tp02.js
+++ tp02.js
@@ -25,26 +25,31 @@ function calcul1(){
     let chiffre2= document.getElementById('chiffre2').value;
     let operateur = document.getElementById('choix').value;
     let total = 0;
+    let word="";
     console.log(typeof(operateur));
     if(chiffre1 && chiffre2 && operateur){
             switch(operateur){
 
                     case '+' :
                          total = parseInt(chiffre1)+parseInt(chiffre2);
+                         word ="Le résultat de l'addition est "
                         break;
                     case '-' :
                         total = parseInt(chiffre1)-parseInt(chiffre2);
+                        word ="Le résultat de la soustraction est "
                         break;
                     case '*' :
                         total = parseInt(chiffre1)*parseInt(chiffre2);
+                        word ="Le résultat de la multiplication est "
                         break; 
                     case '/' :
                         total = parseInt(chiffre1)/parseInt(chiffre2);
+                        word ="Le résultat de la division est "
                         break;     
             }
           
            document.getElementById('resultat').style.backgroundColor = "";
-           document.getElementById('resultat').innerHTML="Le résultat est "+total;
+           document.getElementById('resultat').innerHTML=word+total;
            
     }else if(chiffre1== 0|| chiffre2 ==0){
        console.log("non");
