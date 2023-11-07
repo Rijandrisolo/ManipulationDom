a0e5a2581b94a6f9cf13ebdb747a9dc4c20bd37d
rijarandria <rijandrisolo@gmail.com>
2023-11-07 15:45:32 +0100

    tp05 Affichage Json

--- tp02.js
+++ tp02.js
@@ -48,18 +48,18 @@ function calculOperation(){
                         break;     
             }
           
-           document.querySelector('resultat').style.backgroundColor = "";
-           document.querySelector('resultat').innerHTML=word+total;
+           document.querySelector('#resultat').style.backgroundColor = "";
+           document.querySelector('#resultat').innerHTML=word+total;
            
     }else if(chiffre1== 0|| chiffre2 ==0){
        console.log("non");
-       document.querySelector('resultat').style.backgroundColor = "red";
-       document.querySelector('resultat').innerHTML="Saisir des chiffres ";
+       document.querySelector('#resultat').style.backgroundColor = "red";
+       document.querySelector('#resultat').innerHTML="Saisir des chiffres ";
     }
    
    
    }
-   //////////////
+   ///////////////////////////
 
    function checkFormulaire(){
 
@@ -78,3 +78,42 @@ function calculOperation(){
    
    
    }
+
+   ///////////////////////////////
+
+   function affichageJson(){
+
+    let data = {"polluant":"CO2", "unite":"milliards de tonnes", "annee":2017, 
+    "pays":[
+        {"nom":"Chine", "valeur":9.26, "pourcentage":28.2, "code":"cn"}, 
+        {"nom":"Etats-Unis", "valeur":4.76, "pourcentage":14.5, "code":"us"}, 
+        {"nom":"Inde", "valeur":2.16, "pourcentage":6.6, "code":"in"}, 
+        {"nom":"Russie", "valeur":1.54, "pourcentage":4.7, "code":"ru"}, 
+        {"nom":"Japon", "valeur":1.13, "pourcentage":3.4, "code":"jp"}, 
+        {"nom":"Allemagne", "valeur":0.72, "pourcentage":2.2, "code":"de"}, 
+        {"nom":"Corée du Sud", "valeur":0.6, "pourcentage":1.8, "code":"kr"}, 
+        {"nom":"Iran", "valeur":0.57, "pourcentage":1.7, "code":"ir"}, 
+        {"nom":"Canada", "valeur":0.55, "pourcentage":1.7, "code":"ca"}
+    ]
+    }
+    let polluant = data.polluant;
+    let annee = data.annee;
+    let pays = data.pays;
+    console.log(pays);
+       
+    
+       let e = document.querySelector('#affichage');
+       console.log(e);
+        e.innerHTML = "<h1>" +"Polluant :"+ polluant+ "</h1>";
+        e.innerHTML += "<h2>" +"Année de mesure :"+ annee+ "</h1>";
+   
+        for (let i=0; i<=pays.length;i++){
+            
+            
+            e.innerHTML += "<span>"+ " | "+"Pays : "+ pays[i].nom+" | "+"Valeurs : "+ pays[i].valeur+" | "+"Pourcentage : "+pays[i].pourcentage+"</span>"+"<br>";
+
+
+        }
+
+   }
+
