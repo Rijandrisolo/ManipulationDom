a5e84b88d9a48ef8e8d63b9a984340677378c989
rijarandria <rijandrisolo@gmail.com>
2023-11-08 09:07:40 +0100

    Modification des affichages inner.HTML en backticks

--- tp02.js
+++ tp02.js
@@ -6,9 +6,9 @@ function calculAddition(){
  if(chiffre1 || chiffre2){
         let total = parseInt(chiffre1)+parseInt(chiffre2);
         document.querySelector('#resultat').style.backgroundColor = "";
-        document.querySelector('#resultat').innerHTML="Le résultat est "+total;
+        document.querySelector('#resultat').innerHTML=`Le résultat est ${total}`;
         
- }else if(chiffre1== 0|| chiffre2 ==0){
+ }else if(chiffre1== 0&& chiffre2 ==0){
     console.log("non");
     document.querySelector('#resultat').style.backgroundColor = "red";
     document.querySelector('#resultat').innerHTML="Saisir des chiffres ";
@@ -17,7 +17,9 @@ function calculAddition(){
 
 }
 /////////////////////////////////////////////
-
+/**
+ * calcul
+ */
 
 function calculOperation(){
 
@@ -32,24 +34,24 @@ function calculOperation(){
 
                     case '+' :
                          total = parseInt(chiffre1)+parseInt(chiffre2);
-                         word ="Le résultat de l'addition est "
+                         word =`Le résultat de l'addition est ${total}`;
                         break;
                     case '-' :
                         total = parseInt(chiffre1)-parseInt(chiffre2);
-                        word ="Le résultat de la soustraction est "
+                        word =`Le résultat de la soustraction est ${total}`;
                         break;
                     case '*' :
                         total = parseInt(chiffre1)*parseInt(chiffre2);
-                        word ="Le résultat de la multiplication est "
+                        word =`Le résultat de la multiplication est ${total}`;
                         break; 
                     case '/' :
                         total = parseInt(chiffre1)/parseInt(chiffre2);
-                        word ="Le résultat de la division est "
+                        word =`Le résultat de la division est ${total}`;
                         break;     
             }
           
            document.querySelector('#resultat').style.backgroundColor = "";
-           document.querySelector('#resultat').innerHTML=word+total;
+           document.querySelector('#resultat').innerHTML= word;
            
     }else if(chiffre1== 0|| chiffre2 ==0){
        console.log("non");
@@ -106,14 +108,57 @@ function calculOperation(){
        console.log(e);
         e.innerHTML = "<h1>" +"Polluant :"+ polluant+ "</h1>";
         e.innerHTML += "<h2>" +"Année de mesure :"+ annee+ "</h1>";
-   
+        
+
         for (let i=0; i<=pays.length;i++){
             
             
-            e.innerHTML += "<span>"+ " | "+"Pays : "+ pays[i].nom+" | "+"Valeurs : "+ pays[i].valeur+" | "+"Pourcentage : "+pays[i].pourcentage+"</span>"+"<br>";
+            //e.innerHTML += "<span>"+ " | "+"Pays : "+ pays[i].nom+" | "+"Valeurs : "+ pays[i].valeur+" | "+"Pourcentage : "+pays[i].pourcentage+"</span>"+"<br>";
+            e.innerHTML += `<span> | Pays : ${pays[i].nom} | Valeurs : ${pays[i].valeur} | Pourcentage : ${pays[i].pourcentage} </span> <br>`;
+
+        }
+        
+    }
+///////////////////////////////
+
+/*function affichageJsonTableau(){
+
+    let data = {"polluant":"CO2", "unite":"milliards de tonnes", "annee":2017, 
+    "pays":[
+        {"nom":"Chine", "valeur":9.26, "pourcentage":28.2, "code":"cn"}, 
+        {"nom":"Etats-Unis", "valeur":4.76, "pourcentage":14.5, "code":"us"}, 
+        {"nom":"Inde", "valeur":2.16, "pourcentage":6.6, "code":"in"}, 
+        {"nom":"Russie", "valeur":1.54, "pourcentage":4.7, "code":"ru"}, 
+        {"nom":"Japon", "valeur":1.13, "pourcentage":3.4, "code":"jp"}, 
+        {"nom":"Allemagne", "valeur":0.72, "pourcentage":2.2, "code":"de"}, 
+        {"nom":"Corée du Sud", "valeur":0.6, "pourcentage":1.8, "code":"kr"}, 
+        {"nom":"Iran", "valeur":0.57, "pourcentage":1.7, "code":"ir"}, 
+        {"nom":"Canada", "valeur":0.55, "pourcentage":1.7, "code":"ca"}
+    ]
+    }
+    let polluant = data.polluant;
+    let annee = data.annee;
+    let pays = data.pays;
+    console.log(pays);
+       
+    
+       let e = document.querySelector('#affichage');
+       console.log(e);
+        e.innerHTML = "<h1>" +"Polluant :"+ polluant+ "</h1>";
+        e.innerHTML += "<h2>" +"Année de mesure :"+ annee+ "</h1>";
+        e.innerHTML += "<table border=1">" ;
+        e.innerHTML += "<thead>";
+        e.innerHTML +="<tr>"+"<th>"+ "Pays"+"</th>"+"<th>"+ "Valeur"+"</th>"+"<th>"+ "%"+"</th>"+"</tr>"
+        e.innerHTML += "</thead>";
+       /* e.innerHTML += "<tbody>" ;
+
+        for (let i=0; i<=pays.length;i++){
+            
+            
+            e.innerHTML += "<tr>"+"<td>"+ pays[i].nom+ pays[i].valeur+pays[i].pourcentage+"</td>"+"</tr>";
 
 
         }
-
-   }
-
+        e.innerHTML += "</tbody>" ;
+        e.innerHTML += "</table>" ;
+   }*/
